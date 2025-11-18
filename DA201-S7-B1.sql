07398a405a779959df8df291e75ed028768bd993
nmanh7891-max <nmanh7891@gmail.com>
2025-11-19 01:04:53 +0700

    Add files via upload

---
+++ DA201-S7-B1.sql
@@ -0,0 +1,36 @@
+create database sanpham;
+use sanpham;
+CREATE TABLE san_pham (
+    id INT PRIMARY KEY,
+    ten_san_pham VARCHAR(255),
+    danh_muc VARCHAR(100),
+    gia_ban INT,
+    so_luong_ton INT
+);
+INSERT INTO san_pham (id, ten_san_pham, danh_muc, gia_ban, so_luong_ton) VALUES
+(1, 'Laptop Dell XPS 13', 'Laptop', 28000000, 15),
+(2, 'iPhone 14 Pro Max', 'Điện thoại', 32000000, 30),
+(3, 'Bàn phím cơ Logitech', 'Phụ kiện', 2500000, 50),
+(4, 'Laptop MacBook Air M2', 'Laptop', 29500000, 20),
+(5, 'Samsung Galaxy S23 Ultra', 'Điện thoại', 26000000, 25),
+(6, 'Chuột không dây Fuhlen', 'Phụ kiện', 450000, 100),
+(7, 'Tai nghe Sony WH-1000XM5', 'Phụ kiện', 8500000, 40),
+(8, 'Laptop HP Envy 15', 'Laptop', 24000000, 18);
+select *
+from san_pham
+where gia_ban between 20000000 and 30000000;
+select *
+from san_pham
+where ten_san_pham like '%Laptop%';
+select count(*) as so_luong_phu_kien
+from san_pham
+where danh_muc = 'Phu kien';
+select *
+from san_pham
+order by gia_ban desc
+limit 3;
+
+
+
+
+
